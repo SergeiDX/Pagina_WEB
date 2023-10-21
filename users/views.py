01f17b7c8e56a0865cd885c7186bda964fd97f6e
SergeiDX <sergio_salazar@uadec.edu.mx>
2023-10-21 13:31:49 -0600

    Primer Commit

---
+++ users/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout
+from django.views.generic import View
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+# Create your views here.
+
+class VRegistro(View):
+    def get(self, request):
+        form=UserCreationForm()
+        return render(request,'signup2.html',{"form":form})
+    
+    def post(self, request):
+        form=UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            
+            usuario=form.save()
+        
+            auth_login(request, usuario) 
+            return redirect('home')
+        else:
+            for msg in form.error_messages:
+                messages.error(request, form.error_messages[msg])
+                
+            return render(request,'signup2.html',{"form":form})
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('home')
+    
+    
+def logear(request):
+    form=AuthenticationForm()
+    return render(request, 'login.html',{"form":form})
+
+        
+
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    else:
+        if request.POST['contrasena'] == request.POST['confirmar-contrasena']:
+            try:                
+                user = User.objects.create_user(username=request.POST['nombre-usuario'],password=request.POST['contrasena'])
+                
+                user.save()
+                login(request, user)
+                return redirect('home')  
+              
+            except:
+                return render(request,'signup.html',{"error2":'el usuario ya existe'})
+        return render(request,'signup.html',{"error":'Las contraseñas no coinciden'})
+        
+
+
+def login(request):
+    return render(request, 'login.html')
