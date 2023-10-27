824a92f98a9687cbb44c07ade99abb346cff7c04
SergeiDX <sergio_salazar@uadec.edu.mx>
2023-10-27 17:25:13 -0600

    -Tabla paq,est y envio

--- pedidos/models.py
+++ pedidos/models.py
@@ -3,8 +3,21 @@ from django.contrib.auth import get_user_model
 from tienda.models import Producto
 from django.db.models import F, Sum, FloatField
 # Create your models here.
-
 User = get_user_model()
+
+class Paqueteria(models.Model):
+    nombrepaq = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nombrepaq
+    
+    
+class Estado(models.Model):
+    nombreEST = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nombreEST
+
 class Pedido(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -43,3 +56,35 @@ class LineaPedido(models.Model):
         verbose_name = 'Linea pedido'
         verbose_name_plural = 'Lineas Pedidos'
         ordering = ['id']
+        
+        
+class Envio(models.Model):
+    Nombre = models.CharField(max_length=50)
+    Apellidos = models.CharField(max_length=50)
+    Direccion = models.CharField(max_length=100)
+    Telefono = models.CharField(max_length=40)
+    Codigo_Postal = models.CharField(max_length=50)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
+    paqueteria = models.ForeignKey(Paqueteria, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'Envio'
+        verbose_name= 'Envio'
+        verbose_name= 'Envios'
+        ordering = ['id']
+        
+class Banco(models.Model):
+    nombrebanc= models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nombrebanc
+
+class Tarjeta(models.Model):
+    numero_tarjeta = models.CharField(max_length=50)
+    fecha_exp = models.CharField(max_length=50)
+    cvv = models.CharField(max_length=50)
+    nombrebanco = models.ForeignKey(Banco, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
