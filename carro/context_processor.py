f673a6f51a0d58952b28aaa0bc1bbd8bf6a908e6
SERGIO SALAZAR ZUNIGA <sergio_salazar@uadec.edu.mx>
2023-10-27 13:00:53 -0600

    Correccion variable global total y creacion de app

--- carro/context_processor.py
+++ carro/context_processor.py
@@ -5,5 +5,5 @@ def importe_total_carro(request):
         if "carro" in request.session:
             
             for key, value in request.session["carro"].items():
-                total=total+(float(value["precio"])*value["cantidad"])
+                total=total+float(value["precio"])
     return {"importe_total_carro":total}
